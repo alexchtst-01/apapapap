7feb1bfee8a5e17ad5fe52e66f31d6ee30c6da94
alexchtst-01 <acinatra@gmail.com>
2024-10-29 01:07:31 -0400

    feat: initial project

---
+++ api/index.js
@@ -0,0 +1,78 @@
+// Import dependencies
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config()
+
+const dataResult = new mongoose.Schema(
+  {
+    humidity: {
+      type: Number,
+      required: true,
+    },
+    temp: {
+      type: Number,
+      required: true,
+    },
+    gas: {
+      type: Number,
+      required: true,
+    },
+    device: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Data = mongoose.model("data", dataResult);
+
+export const postData = async (req, res) => {
+  const { humidity, temp, gas, device } = req.body;
+  try {
+    const newData = new Data({ humidity, temp, gas, device });
+    await newData.save();
+    res.status(201).json({ message: "Data posted successfully", data: newData });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to post data", error: error.message });
+  }
+};
+
+const app = express();
+app.use(cors(
+  {origin: "*"}
+));
+app.use(express.json());
+
+const router = express.Router();
+
+router.get("/", () => {
+    res.send("hallo from backend")
+})
+
+router.post("/data", postData);
+
+const PORT = process.env.PORT || 5000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.CONN_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB Atlas");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+connectDB();
